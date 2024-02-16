bdb7dfe8601946f10dc6a765eff5a9ba95a08d4b
João Victor <101882992+joaovictor31@users.noreply.github.com>
2024-02-16 17:23:35 -0300

    Add files via upload

---
+++ exercicio_002/app.js
@@ -0,0 +1,10 @@
+/* ----------------------------------------------------------------------------
+
+Exercício: 002
+Enunciado: CONTADOR COM INTERVALO DE VALORES
+    
+    A partir do HTML existente, apresentar um valor inicial igual a 0 e definir
+    funcionalidades nos botões de decremento e incremento.
+    O valor nunca poderá ser inferior a -10 ou superior a 10.
+
+---------------------------------------------------------------------------- */
