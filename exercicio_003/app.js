1b8e6c51e8b680880d9cf2c18543eec18a0d4ab7
joao <ut7vitor60@gmail.com>
2024-02-17 10:46:30 -0300

    Exercicio 03 concluido

--- exercicio_003/app.js
+++ exercicio_003/app.js
@@ -10,30 +10,42 @@ Enunciado: CONTADOR COM CORES PARA NEGATIVOS, ZERO E POSITIVOS
     No caso do valor ser positivo o texto deve ser verde puro.
 
 ---------------------------------------------------------------------------- */
-valor = 0 
+let valor = 0 
 const h1_valor = document.getElementById('valor');
 const adicionar = document.getElementById('btn_incremento');
 const remover = document.getElementById('btn_decremento');
 
-
-
 function add(){
-    if (valor >= 0){
-        valor += 1;
-        h1_valor.textContent = valor;
-        h1_valor.style.color = 'green';
-    }
-
+    ++valor;
+    set_valor()
 }
 
 function dele(){
-    if(valor <= 0){
-        valor -= 1;
-        h1_valor.textContent = valor;
+    --valor;
+    set_valor()
+}
+
+function set_valor(){
+    h1_valor.textContent = valor;
+    if (valor === 0){
+        h1_valor.style.color = 'white';
+    } else if (valor > 0){
+        h1_valor.style.color = 'green';
+    } else{
         h1_valor.style.color = 'red';
     }
-    
 }
 
 adicionar.addEventListener('click',add);
 remover.addEventListener('click',dele);
+
+/* Outra alternativa de resolver usando o ternário 
+
+function set_valor(){
+    h1_valor.textContent = valor;
+    let color = valor == 0 ? 'white' : valor < 0 ? 'red' : 'green';
+    h1_valor.style.color = color;
+    
+}
+
+*/
